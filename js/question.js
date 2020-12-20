2eae6845802e5998b2c20a2019c354a2788fcbfd
TamilElakkiya <tamilillakiyag510@gmail.com>
2020-12-20 12:18:30 +0530

    My_first_project

---
+++ js/question.js
@@ -0,0 +1,55 @@
+
+
+//Array of objects
+const quiz = [
+  {
+	q:'How many weeks are there in one year?',
+	options:['58','55','54','52'],
+	answer:3
+  },
+  {
+	q:'Which planet is known as the Red Planet?',
+	options:['Mars','Mercury','Venus','Jupiter'],
+	answer:0
+  },
+  {
+	q:'Human body have two lungs.True or False',
+	options:['True','False'],
+ 	answer:0
+  },
+  {
+	q:'Which day is observed as World Environment Day?',
+	options:['June 8','June 5','June 10','June 15'],
+	answer:1
+  },
+  {
+	q:'What color symbolizes peace?',
+	options:['Green','Yellow','Blue','White'],
+	answer:3
+  },
+  {
+	q:'Global warming is caused by the excess of which type of gas?',
+	options:['Neon','Hydrogen','Carbon dioxide','Oxygen'],
+	answer:2
+  },
+  {
+	q:'Which is the biggest desert in the world?',
+	options:['Thar Desert','Sahara desert','Sonoran Desert','Gobi Desert'],
+	answer:1
+  },
+  {
+	q:'Which is the longest written Constitution in the world?',
+	options:['India','America','Japan','China'],
+	answer:0
+  },
+  {
+	q:'Which parts of the body continue to grow for your entire life?',
+	options:['Nose','Hand','Mouth','Ears'],
+	answer:3
+  },
+  {
+	q:'William Shakespeare is a scientist',
+	options:['True','False'],
+	answer:1
+  },
+]
